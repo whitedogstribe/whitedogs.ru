64838b3385954c7ad0897f6b227c9f6de4a96d67
Эрадж Шамс <eradzh@MacBook-Air-Eradz.local>
2026-07-11 18:28:33 +0400

    Fix config.core.php: update MODX_CORE_PATH to whitedogs.ru

--- config.core.php
+++ config.core.php
@@ -4,6 +4,6 @@
  * Add customizations to the $config_options array in `core/config/config.inc.php`.
  *
  */
-define('MODX_CORE_PATH', '/var/www/whitedogs_bo_usr/data/www/whitedogs.boshnik.com/core/');
+define('MODX_CORE_PATH', '/var/www/whitedogs.ru/core/');
 define('MODX_CONFIG_KEY', 'config');
 ?>
